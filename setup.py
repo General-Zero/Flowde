0b0182066f0c608552232e0d6ecd72f064842ff0
General-Zero <jayraldvax@gmail.com>
2024-10-21 08:33:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,20 +9,17 @@ setuptools.setup(
 	    long_description_content_type='text/markdown',
 	    author='General Zero',
 	    author_email='jayraldvax@gmail.com',
-	    license='Apache License 2.0',
+	    license='Apache Software License 2.0',
 	    classifiers=[
 	    "Intended Audience :: Developers",
 	    "Programming Language :: Python :: 3.10",
 	    "Programming Language :: Python :: 3.11",
-    "License :: OSI Approved :: MIT License",
-    "Topic :: Utilities"
+            "License :: OSI Approved :: Apache Software License 2.0",
+            "Topic :: Utilities"
 	    ],
 	    python_requires='>=3.10',
 	    install_requires=['requests'],
 	    packages=setuptools.find_packages(),
 	    include_package_data=True,
-	    entry_points={
-    "console_scripts": [
-        "flowde = flowde.cli:main"]
-},
+	    entry_points={"console_scripts": ["flowde = flowde.cli:main"]},
 )
