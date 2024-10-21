9d1b55578dcf9c2c185be6cfdb35a1beb12cd510
General-Zero <jayraldvax@gmail.com>
2024-10-21 17:53:29 +0800

    Update func.py

--- flowde/func.py
+++ flowde/func.py
@@ -2,3 +2,7 @@ def text(textinput):
     print(textinput)
 def num(numinput):
     print(int(numinput))
+def radnum(value):
+    specifynum = input('Specify your value')
+    if value > specifynum:
+        print(value == specifynum)
