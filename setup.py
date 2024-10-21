4db22391b3911807d105f1935c005e4ad05c35bf
General-Zero <jayraldvax@gmail.com>
2024-10-21 08:25:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setuptools.setup(
 	    name='flowde',
-	    version='0.0.1',
-	    description='Flowde is a simple PY library.',
+	    version='0.0.2',
+	    description='Flowde is a simple PY library to help you out with coding in python.',
 	    long_description=pathlib.Path('README.md').read_text(),
 	    long_description_content_type='text/markdown',
-	    author='Cosmo',
+	    author='General Zero',
 	    author_email='jayraldvax@gmail.com',
-	    license='MIT License',
+	    license='Apache License 2.0',
 	    classifiers=[
 	    "Intended Audience :: Developers",
 	    "Programming Language :: Python :: 3.10",
