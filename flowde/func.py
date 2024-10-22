8843cc016aeb2c2a49f466aafd7158572881abc4
General-Zero <jayraldvax@gmail.com>
2024-10-22 15:02:01 +0800

    Update func.py

--- flowde/func.py
+++ flowde/func.py
@@ -7,3 +7,20 @@ def ttkn(text):
     amount = text.split()
     ttkn = len(amount)
     print(ttkn)
+def help(syntax=None):
+    help_syntax = {
+        'print()': 'The print() snippet outputs text or the value of a variable.',
+        'def func():': 'The "def" snippet defines a function which is similar to "function fname() {}" in JavaScript if you are a JavaScript developer',
+    }
+    
+    if syntax in help_syntax:
+    	print(Fore.CYAN + help_syntax[syntax])
+    elif syntax is None:
+    	print(Fore.GREEN + f'Visit https://github.com/General-Zero/flowde-documentation for more info about Flowde or Functions.')
+    	print()
+    	print(Fore.GREEN + f'Visit the official python beginner\'s guide for more info: https://www.python.org/about/gettingstarted/')
+    else:
+    	print(Fore.RED + f'The syntax "{syntax}" is undefined or does not exist in Python.')
+    	print()
+    	print(Fore.YELLOW + f'Perhaps the syntax is missing something?')
+        print(Style.RESET_ALL)
