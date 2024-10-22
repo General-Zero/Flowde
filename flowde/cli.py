b6957704913c3bd5accaffa2dd17f58e154d51ed
General-Zero <jayraldvax@gmail.com>
2024-10-22 18:40:40 +0800

    Update cli.py

--- flowde/cli.py
+++ flowde/cli.py
@@ -2,17 +2,22 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--version', action='store_true', help='Display Flowde version')
-    parser.add_argument('--phase', action="store_true", help="Displays Dev stage Of Flowde")
+    parser.add_argument('--devst', action="store_true", help="Displays the development stage of Flowde")
+    parser.add_argument('--dependencies', action='store_true', help='Dependencies installed')
 
     args = parser.parse_args()
 
     if args.version:
         version()
-    elif args.phase:
-        phase()
+    elif args.devst:
+        devst()
+    elif args.dependencies:
+        dependencies()
     else:
         print("Flowde has successfully been installed")
 def version():
-    print("Flowde 0.0.1")
-def phase():
-    print("Alpha")
+    print("Flowde 0.0.4")
+def devst():
+    print("Alpha version 0.0.4")
+def dependencies():
+    print('Dependencies installed for Flowde are: [colorama]')
