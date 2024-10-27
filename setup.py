29a951c3ab9d22a51e410012c562efc733f9efae
General-Zero <jayraldvax@gmail.com>
2024-10-27 14:01:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
 	    name='flowde',
-	    version='0.0.3',
+	    version='0.0.4',
 	    description='Flowde is a simple PY library to help you out with coding in python.',
 	    long_description=pathlib.Path('README.md').read_text(),
 	    long_description_content_type='text/markdown',
@@ -12,11 +12,13 @@ setuptools.setup(
 	    license='Apache Software License 2.0',
 	    classifiers=[
 	    "Intended Audience :: Developers",
+	    "Natural Language :: English",
+	    "Programming Language :: Python :: 3",
 	    "Programming Language :: Python :: 3.10",
 	    "Programming Language :: Python :: 3.11",
 	    "Development Status :: 3 - Alpha",
-            "License :: OSI Approved :: Apache Software License",
-            "Topic :: Utilities",
+	    "License :: OSI Approved :: Apache Software License",
+            "Topic :: Utilities"
 	    ],
 	    python_requires='>=3.10',
 	    install_requires=['requests', 'colorama'],
