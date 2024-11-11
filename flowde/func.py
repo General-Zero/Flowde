a5eace3a94db56d7f9805d9a33940b89950c0671
General-Zero <jayraldvax@gmail.com>
2024-11-11 20:51:02 +0800

    Update func.py

--- flowde/func.py
+++ flowde/func.py
@@ -1,5 +1,8 @@
 from colorama import Fore, Back, init
-import
+import requests
+from flask import Flask
+import datetime
+import json
 init(autoreset=True)
 def text(textinput):
     print(textinput)
@@ -10,7 +13,7 @@ def ttkn(text):
     amount = text.split()
     ttkn = len(amount)
     print(ttkn)
-def flowdehelp(option=None):
+def help(option=None):
     options = {
         'py3mtp': 'Let\'s get to the basic syntax of python:\nprint() prints text or the value of a variable.\n\ndef your_func_name(parameters as needed):\n# your_codecode\nchange \'your_func_name\' and \'parameters as needed\'\nwith you\'re function and parameters name. Parameters will be\nexplained shortly.\nParameters - Parameters is you\'re value or input from a\nfunction, for example:\ndef add(a, b):\nprint(a + b) - You can also use \`return\` instead of \`print\` to return that\noutput instead of printing it.'
     }
@@ -20,3 +23,17 @@ def flowdehelp(option=None):
       print(Fore.GREEN + 'No options provided, try:\npy3mtp')
     else:
       print(Fore.RED + f'{option} option does not exist, perhaps you misspelled it?')
+def capi(url):
+    r = requests.get(url):
+    x = r.status_code
+    try:
+        json = r.json()
+        data = json.dumps(json)
+        print(data)
+    except ValueError:
+        print('Encoding: ' r.encoding)
+        print(r.text)
+    except requests.RequestException as Rex:
+        print(f'An error occured during requesting {url}: ', Rex)
+    except requests.HTTPError as herror:
+        print('HTTP error: ', herror)
